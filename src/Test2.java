74806d508d1524e6b4056f859f7d10ed0b636882
haroldCoder <haroldc2005@gmail.com>
2023-11-08 19:35:17 -0500

    terminate test 2

--- src/Test2.java
+++ src/Test2.java
@@ -1,5 +1,6 @@
 import java.util.List;
 
+import org.checkerframework.checker.units.qual.t;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
@@ -7,15 +8,20 @@ import org.openqa.selenium.WebElement;
 import dev.failsafe.internal.util.Assert;
 
 
-
 public class Test2 extends App{
-   @Override
+   String email;
+   String password;
+
+
    public void Newverifiuser(){
       this.clicksiginup();
-      List<WebElement> txtemail = this.driver.findElements(By.name("email"));
-        txtemail.get(0).sendKeys("elkoderselagano@gmail.com");
+      this.email = "elkoderselagano@gmail.com";
+      this.password = "1234";
 
-        this.driver.findElement(By.name("password")).sendKeys("1234");
+      List<WebElement> txtemail = this.driver.findElements(By.name("email"));
+        txtemail.get(0).sendKeys(this.email);
+
+        this.driver.findElement(By.name("password")).sendKeys(this.password);
 
         List<WebElement> btnsiginup = driver.findElements(By.xpath("//button[contains(@class,'btn-default')]"));
         btnsiginup.get(0).click();
@@ -26,5 +32,35 @@ public class Test2 extends App{
    public void VerifyLogin(){
       this.clicksiginup();
       junit.framework.Assert.assertTrue(this.driver.findElement(By.className("login-form")).isDisplayed());
+   }
+
+   public void userExist(){
+      this.Newverifiuser();
+      try{
+         WebElement msgexist = this.driver.findElement(By.xpath("//p[contains(., 'Your email or password is incorrect!')]"));
+      
+         if(msgexist.isDisplayed()){
+            this.Newverifiuser(this.email, this.password, true);
+            this.Newverifiuser();
+         }
+      }
+      catch(Exception e){
+         this.driver.findElements(By.className("btn btn-default")).get(0).click();
+      }
+   }
+
+   @Test
+   public void VisibleUsername(){
+      userExist();
+      WebElement username = driver.findElement(By.xpath("//i[contains(., ' Logged in as ')]"));
+      junit.framework.Assert.assertTrue(username.isDisplayed());
+      this.DeleteAccount();
+   }
+
+   @Test
+   public void VerifyAccountDelete(){
+      this.VisibleUsername();
+      WebElement username = driver.findElement(By.xpath("//b[contains(., 'Account Deleted!')]"));
+      junit.framework.Assert.assertTrue(username.isDisplayed());
    }
 }
